39af1e8f07ef2eca5a449cd96ec442c614dc5482
mvorxan <117027967+mvorxan@users.noreply.github.com>
2023-06-11 03:54:28 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,7 +14,7 @@ let usernames = new Map();
 // START
 $(document).ready(() => {
     setTimeout(function () {
-        let targetLive = "game_live_az";
+        let targetLive = "oyun_aze";
         connect(targetLive);
     }, 5000);
 
