f51678b33a362737fc550731c2c922fb11dd8658
mvorxan <117027967+mvorxan@users.noreply.github.com>
2023-06-30 00:44:49 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -416,9 +416,9 @@ connection.on('like', (data) => {
         let totalLikeCount = data.totalLikeCount;
     if (isPaused) return;
 
-       if(Math.floor(totalLikeCount / 1000) > Math.floor(previousLikeCount / 1000)) {
-        playSpecificSound(23);
-    }
+    //    if(Math.floor(totalLikeCount / 1000) > Math.floor(previousLikeCount / 1000)) {
+    //     playSpecificSound(23);
+    // }
 
     messagesQueue = messagesQueue.filter(item => item.type !== 'random');
     const messages = [
