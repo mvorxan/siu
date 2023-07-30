f42f32cd818479af4e8416283b5150fdbbffb562
mvorxan <117027967+mvorxan@users.noreply.github.com>
2023-07-31 02:35:47 +0400

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -81,7 +81,7 @@ let usernames = new Map();
 // START
 $(document).ready(() => {
     setTimeout(function () {
-        let targetLive = "spektraauto326";
+        let targetLive = "freecoinups";
         connect(targetLive);
     }, 5000);
 
